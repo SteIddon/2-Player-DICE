67153afd9bc651bcad4c12c46769775f552efe9a
Ste Iddon <steiddon@gmail.com>
2021-02-04 16:14:01 +0000

    Commit 3
    Game now functional for 2 players, all buttons work as intended and styling updated

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ let oneTotal = document.getElementById("onetotal");
 let twoTotal = document.getElementById("twototal");
 let holdScore = document.getElementById("hold");
 let newGame = document.getElementById("newgame");
+let divOne = document.getElementById("playerone");
+let divTwo = document.getElementById("playertwo");
+let turnCount = 0;
 let diceArr1 = [];
 let diceArr2 = [];
 
 function rollDice() {
     let rollResult = Math.floor(Math.random() * 6) + 1;
     if (rollResult == 1) {
-        oneCurrent.textContent = "You lose! Try Again"
+        oneCurrent.textContent = "You lose!"
         oneTotal.textContent = "0"
         diceArr1 = [];      
         rollImage.innerHTML = '<img src="dice' + rollResult + '.png">';  
@@ -32,12 +35,55 @@ function rollDice() {
 }
 }
 diceRoll.addEventListener("click", () => {
-    rollDice();
+    if (turnCount === 0) {
+        rollDice();
+    } else {
+        rollDiceTwo();
+    }
 })
 newGame.addEventListener("click", () => {
+    if (turnCount === 1) {
+        turnCount = 0
+        divOne.style.backgroundColor = "gold"
+        divTwo.style.backgroundColor = "white"
+    }
     diceArr1 = [];
     oneTotal.textContent = ""
     oneCurrent.textContent = ""
     twoTotal.textContent = ""
     twoCurrent.textContent = ""
 })
+
+function rollDiceTwo() {
+    let rollResult = Math.floor(Math.random() * 6) + 1;
+    if (rollResult == 1) {
+        twoCurrent.textContent = "You lose!"
+        twoTotal.textContent = "0"
+        diceArr2 = [];      
+        rollImage.innerHTML = '<img src="dice' + rollResult + '.png">';  
+    } else {
+        twoCurrent.textContent = "Current Roll:   " + rollResult
+        diceArr2.push(rollResult);
+    let sum2 = diceArr2.reduce((a, b) => {
+            return a + b;
+    });
+    twoTotal.textContent = "Total:   " + sum2
+    console.log(sum2)
+    if (sum2 >= 20) {
+        twoTotal.textContent = "Total:    " + sum2 + "     You win!"
+        diceArr2 = [];    
+    }
+    rollImage.innerHTML = '<img src="dice' + rollResult + '.png">';    
+}
+}
+holdScore.addEventListener("click", () => {
+    if (turnCount === 0) {
+        divOne.style.backgroundColor = "white"
+        divTwo.style.backgroundColor = "gold"
+        turnCount = 1
+    } else {
+        divOne.style.backgroundColor = "gold"
+        divTwo.style.backgroundColor = "white"
+        turnCount = 0
+    }
+});
